47d5442abc92d55ab194b9bb8479a32b9e953e47
singhsduos <ns9628491678@gmail.com>
2021-09-05 12:52:22 +0530

    heading-update

--- src/App.js
+++ src/App.js
@@ -7,21 +7,17 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        
-        <div className="space">
-          <div className="space1"></div>
-          <div className="space2"></div>
+        <div className="container">
+          <div className="space">
+            <div className="space1"></div>
+            <div className="space2"></div>
+          </div>
+
+          <h1 id="heading"><span role="img" aria-label="movie camera emoji">
+            🎥
+          </span>  Movie And Series Recommendation App
+          </h1>
         </div>
-
-        <h1 id="heading"><span role="img" aria-label="movie camera emoji">
-          🎥
-        </span>  Movie And Series Recommendation App
-        </h1>
-      </div>
-
-      <div className="subHeadings">
-    
-      
       </div>
     </div>
   );
