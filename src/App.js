1bbf5315bd47906f3fb90453a7e8ff893019d315
singhsduos <ns9628491678@gmail.com>
2021-09-06 15:18:03 +0530

    css-in-description-error

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ var movieList = {
       cast: "Amitabh Bachchan, Taapsee Panu",
       rating: "7.8/10",
       year: "2019",
-      description: "A dynamic young entrepreneur finds herself locked in a hotel room with the corpse of her dead lover. She hires a prestigious lawyer to defend her and they work together to figure out what actually happened.",
-      image: "https://m.media-amazon.com/images/M/MV5BYjZiMzIzYTctNDViZi00OWNmLWFmN2YtMmI2OWJiZWViMmY3XkEyXkFqcGdeQXVyNTYwMzA0MTM@._V1_.jpg"
-
+      description: "A dynamic young entrepreneur finds herself locked in a hotel room with the corpse of her dead lover. She hires a prestigious lawyer to defend her.",
+      image: "https://m.media-amazon.com/images/M/MV5BYjZiMzIzYTctNDViZi00OWNmLWFmN2YtMmI2OWJiZWViMmY3XkEyXkFqcGdeQXVyNTYwMzA0MTM@._V1_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=mSlgu8AQAd4",
     },
 
     {
@@ -24,7 +24,8 @@ var movieList = {
       rating: "7.7/10",
       year: "2015",
       description: "While investigating the disappearance of a chemist, a detective uncovers a larger conspiracy to unsettle Calcutta.",
-      image: "https://m.media-amazon.com/images/M/MV5BYTU1N2VmODMtMjQxZi00ZGRmLWIyZTEtMjg3YmNkZDNjNTc1XkEyXkFqcGdeQXVyNjc5Mjg4Nzc@._V1_.jpg"
+      image: "https://m.media-amazon.com/images/M/MV5BYTU1N2VmODMtMjQxZi00ZGRmLWIyZTEtMjg3YmNkZDNjNTc1XkEyXkFqcGdeQXVyNjc5Mjg4Nzc@._V1_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=ZG--GOpi_0g",
     },
 
 
@@ -34,8 +35,9 @@ var movieList = {
       cast: "Joaquin Phoenix",
       rating: "8.4/10",
       year: "2019",
-      description: "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.",
-      image: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/924607fa629851bc686d925ab8a63e70_480x.progressive.jpg?v=1573572635"
+      description: "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime.",
+      image: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/924607fa629851bc686d925ab8a63e70_480x.progressive.jpg?v=1573572635",
+      youtubeLink: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
     },
 
     {
@@ -45,7 +47,8 @@ var movieList = {
       rating: "7.4/10",
       year: "2014",
       description: "An ex-hit-man comes out of retirement to track down the gangsters that killed his dog and took everything from him.",
-      image: "https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_SL1500_.jpg"
+      image: "https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_SL1500_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=C0BMx-qxsP4",
     },
 
     {
@@ -54,8 +57,9 @@ var movieList = {
       cast: "Daniel Craig",
       rating: "8.0/10",
       year: "2006",
-      description: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
-      image: "https://m.media-amazon.com/images/I/617AgXANWGL._AC_SL1024_.jpg"
+      description: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007.",
+      image: "https://m.media-amazon.com/images/I/617AgXANWGL._AC_SL1024_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=36mnx8dBbGE",
     }
   ],
 
@@ -66,8 +70,9 @@ var movieList = {
       cast: "Paul Rudd",
       rating: "7.3/10",
       year: "2015",
-      description: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help his mentor, Dr. Hank Pym, pull off a plan that will save the world.",
-      image: "https://m.media-amazon.com/images/I/51jqtx8KltL._AC_.jpg"
+      description: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, and save the world.",
+      image: "https://m.media-amazon.com/images/I/51jqtx8KltL._AC_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=pWdKf3MneyI",
     },
 
 
@@ -77,8 +82,9 @@ var movieList = {
       cast: "Dwayne Johnson, Jason Statham, Idris Elba",
       rating: "6.4/10",
       year: "2019",
-      description: "Lawman Luke Hobbs (Dwayne 'The Rock' Johnson) and outcast Deckard Shaw (Jason Statham) form an unlikely alliance when a cyber-genetically enhanced villain threatens the future of humanity.",
-      image: "https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg"
+      description: "Lawman Hobbs ('The Rock') and outcast Deckard Shaw (Jason Statham) form an unlikely alliance to save people from cyber-genetically villan.",
+      image: "https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=HZ7PAyCDwEg",
     },
 
     {
@@ -88,7 +94,8 @@ var movieList = {
       rating: "6.1/10",
       year: "2019",
       description: "Six individuals from all around the globe, each the very best at what they do, have been chosen not only for their skill, but for a unique desire to delete their pasts to change the future.",
-      image: "https://m.media-amazon.com/images/I/61Sqs2aF8fL._AC_SY879_.jpg"
+      image: "https://m.media-amazon.com/images/I/61Sqs2aF8fL._AC_SY879_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=YLE85olJjp8",
     },
 
     {
@@ -97,18 +104,20 @@ var movieList = {
       cast: "Chris Hemsworth,Randeep Hooda",
       rating: "6.7/10",
       year: "2020",
-      description: "Tyler Rake, a fearless black market mercenary, embarks on the most deadly extraction of his career when he's enlisted to rescue the kidnapped son of an imprisoned international crime lord.",
-      image: "https://m.media-amazon.com/images/I/71lpcnTVFXL._SL1500_.jpg"
+      description: "Tyler Rake, a fearless black market mercenary, embarks on the most deadly extraction of his career when he's enlisted to rescue the kidnapped kid.",
+      image: "https://m.media-amazon.com/images/I/71lpcnTVFXL._SL1500_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=L6P3nI6VnlY",
     },
 
     {
-      name: "Pirates of the Caribbean",
+      name: "Pirates Caraibes",
       director: "Joachim RønningEspen Sandberg",
       cast: "Johnny Depp",
       rating: "6.5/10",
       year: "2017",
-      description: "Captain Jack Sparrow is pursued by old rival Captain Salazar and a crew of deadly ghosts who have escaped from the Devil's Triangle. They're determined to kill every pirate at sea...notably Jack.",
-      image: "https://m.media-amazon.com/images/I/61OYUw-EJLL._AC_.jpg"
+      description: "Captain Jack fight with Captain Salazar and a crew of deadly ghosts who have escaped from the Devil's Triangle.",
+      image: "https://m.media-amazon.com/images/I/61OYUw-EJLL._AC_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=Hgeu5rhoxxY",
     },
 
     {
@@ -118,7 +127,8 @@ var movieList = {
       rating: "7.9/10",
       year: "2008",
       description: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
-      image: "https://m.media-amazon.com/images/I/51WoeTa7ZHL._AC_.jpg"
+      image: "https://m.media-amazon.com/images/I/51WoeTa7ZHL._AC_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=8ugaeA-nMTc",
     },
   ],
 
@@ -130,7 +140,8 @@ var movieList = {
       rating: "8.2/10",
       year: "2000",
       description: "Three unemployed men look for answers to all their money problems - but when their opportunity arrives, will they know what to do with it?",
-      image: "https://m.media-amazon.com/images/M/MV5BNDU3NDU5YmUtOWUwZC00NDczLTg4NGEtNGJiNDU5ZDgxZDY1XkEyXkFqcGdeQXVyODMyODMxNDY@._V1_.jpg"
+      image: "https://m.media-amazon.com/images/M/MV5BNDU3NDU5YmUtOWUwZC00NDczLTg4NGEtNGJiNDU5ZDgxZDY1XkEyXkFqcGdeQXVyODMyODMxNDY@._V1_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=Nj4H-X3FAWU",
     },
 
     {
@@ -139,8 +150,9 @@ var movieList = {
       cast: "Aamir Khan, Madhavan, Sharman Joshi",
       rating: "8.4/10",
       year: "2009",
-      description: "Two friends are searching for their long lost companion. They revisit their college days and recall the memories of their friend who inspired them to think differently, even as the rest of the world called them 'idiots'.",
-      image: "https://m.media-amazon.com/images/M/MV5BNTkyOGVjMGEtNmQzZi00NzFlLTlhOWQtODYyMDc2ZGJmYzFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg"
+      description: "Two friends are for searching their long lost companion, who inspired them to think differently.",
+      image: "https://m.media-amazon.com/images/M/MV5BNTkyOGVjMGEtNmQzZi00NzFlLTlhOWQtODYyMDc2ZGJmYzFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=K0eDlFX9GMc",
     },
 
     {
@@ -149,8 +161,9 @@ var movieList = {
       cast: "Sharman Joshi, Kunal Khemu, Tusshar Kapoor, Rajpal Yadav",
       rating: "6.4/10",
       year: "2007",
-      description: "Four lazy friends try to woo a rich girl, in order to live a wealthy lifestyle. Their plan, however goes horribly wrong after they get embroiled with a notorious criminal.",
-      image: "https://m.media-amazon.com/images/M/MV5BOTRiZDE0MjMtMDA0MS00MzU4LWFhMDctYTYwMmU2ZDFmMjViXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg"
+      description: "Four lazy friends try to woo a rich girl, in order to live a wealthy lifestyle.",
+      image: "https://m.media-amazon.com/images/M/MV5BOTRiZDE0MjMtMDA0MS00MzU4LWFhMDctYTYwMmU2ZDFmMjViXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=vqH2cV1T9WE",
     },
 
     {
@@ -160,7 +173,8 @@ var movieList = {
       rating: "8.0/10",
       year: "2016",
       description: "A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks.",
-      image: "https://m.media-amazon.com/images/I/51qRj51ht8L._AC_.jpg"
+      image: "https://m.media-amazon.com/images/I/51qRj51ht8L._AC_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=ONHBaC-pfsk",
     },
 
     {
@@ -170,7 +184,8 @@ var movieList = {
       rating: "7.0/10",
       year: "1999",
       description: "Four teenage boys enter a pact to lose their virginity by prom night.",
-      image: "https://m.media-amazon.com/images/I/61HqHOUj08L._AC_SL1500_.jpg"
+      image: "https://m.media-amazon.com/images/I/61HqHOUj08L._AC_SL1500_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=iUZ3Yxok6N8",
     },
   ],
 
@@ -182,7 +197,8 @@ var movieList = {
       rating: "8.6/10",
       year: "2014",
       description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-      image: "https://m.media-amazon.com/images/I/91obuWzA3XL._AC_SL1500_.jpg"
+      image: "https://m.media-amazon.com/images/I/91obuWzA3XL._AC_SL1500_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=zSWdZVtXT7E",
     },
 
 
@@ -193,7 +209,8 @@ var movieList = {
       rating: "8.8/10",
       year: "2010",
       description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
-      image: "https://m.media-amazon.com/images/I/5103Iag9c9L._AC_.jpg"
+      image: "https://m.media-amazon.com/images/I/5103Iag9c9L._AC_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=YoHD9XEInc0",
     },
 
     {
@@ -203,7 +220,8 @@ var movieList = {
       rating: "8.0/10",
       year: "2015",
       description: "An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.",
-      image: "https://m.media-amazon.com/images/I/A1rjuidemEL._AC_SL1500_.jpg"
+      image: "https://m.media-amazon.com/images/I/A1rjuidemEL._AC_SL1500_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=ej3ioOneTy8",
     },
 
 
@@ -214,7 +232,8 @@ var movieList = {
       rating: "7.5/10",
       year: "2011",
       description: "A substance designed to help the brain repair itself gives advanced intelligence to a chimpanzee who leads an ape uprising.",
-      image: "https://m.media-amazon.com/images/I/81yWc81CqmL._AC_SL1500_.jpg"
+      image: "https://m.media-amazon.com/images/I/81yWc81CqmL._AC_SL1500_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=T3tidwW1gGM",
     },
 
     {
@@ -224,7 +243,8 @@ var movieList = {
       rating: "7.7/10",
       year: "2013",
       description: "Two astronauts work together to survive after an accident leaves them stranded in space.",
-      image: "https://m.media-amazon.com/images/I/81DNIBr-eSL._AC_SL1500_.jpg"
+      image: "https://m.media-amazon.com/images/I/81DNIBr-eSL._AC_SL1500_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=OiTiKOy59o4",
     }
   ],
 
@@ -235,8 +255,9 @@ var movieList = {
       cast: "Grant Gustin, Candice Patton, Tom Cavanagh",
       rating: "7.6/10",
       year: "2014",
-      description: "After being struck by lightning, Barry Allen wakes up from his coma to discover he's been given the power of super speed, becoming the next Flash, fighting crime in Central City.",
-      image: "https://m.media-amazon.com/images/I/51SaO9oxn5L._AC_.jpg"
+      description: "After being struck by lightning, Barry Allen become the next Flash with super speed power, fighting crime in Central City.",
+      image: "https://m.media-amazon.com/images/I/51SaO9oxn5L._AC_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=Yj0l7iGKh8g",
     },
 
 
@@ -247,7 +268,8 @@ var movieList = {
       rating: "8.3/10",
       year: "2017",
       description: "An unusual group of robbers attempt to carry out the most perfect robbery in Spanish history - stealing 2.4 billion euros from the Royal Mint of Spain.",
-      image: "https://m.media-amazon.com/images/I/61boFr6SYZL._SL1000_.jpg"
+      image: "https://m.media-amazon.com/images/I/61boFr6SYZL._SL1000_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=8QkyLqmJCMM",
     },
 
     {
@@ -256,8 +278,9 @@ var movieList = {
       cast: "Brad Swaile, Ryô Naitô, Brian Drummond",
       rating: "9.0/10",
       year: "2006",
-      description: "An intelligent high school student goes on a secret crusade to eliminate criminals from the world after discovering a notebook capable of killing anyone whose name is written into it.",
-      image: "https://m.media-amazon.com/images/M/MV5BODkzMjhjYTQtYmQyOS00NmZlLTg3Y2UtYjkzN2JkNmRjY2FhXkEyXkFqcGdeQXVyNTM4MDQ5MDc@._V1_.jpg"
+      description: "An intelligent high school student goes on a secret crusade to eliminate criminals by discovering a super powerful notebook by writing names.",
+      image: "https://m.media-amazon.com/images/M/MV5BODkzMjhjYTQtYmQyOS00NmZlLTg3Y2UtYjkzN2JkNmRjY2FhXkEyXkFqcGdeQXVyNTM4MDQ5MDc@._V1_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=NlJZ-YgAt-c",
     },
 
     {
@@ -266,18 +289,20 @@ var movieList = {
       cast: "Manoj Bajpai, Samantha Akkineni, Sharib Hashmi",
       rating: "8.8/10",
       year: "2019",
-      description: "A working man from the National Investigation Agency tries to protect the nation from terrorism, but he also needs to keep his family safe from his secret job.",
-      image: "https://m.media-amazon.com/images/M/MV5BMzRjZWVmMzItNTdmYS00OWEzLTgyOGUtNThiNTU2ZThlYjY0XkEyXkFqcGdeQXVyMTIxMDk2NDE4._V1_.jpg"
+      description: "A working man from the National Investigation Agency tries to protect the nation from terrorism.",
+      image: "https://m.media-amazon.com/images/M/MV5BMzRjZWVmMzItNTdmYS00OWEzLTgyOGUtNThiNTU2ZThlYjY0XkEyXkFqcGdeQXVyMTIxMDk2NDE4._V1_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=XatRGut65VI",
     },
 
     {
       name: "Mirzapur",
-      director: "Puneet Krishna, Karan Anshuman",
+      director: "Puneet Krishna",
       cast: "Pankaj Tripathi, Ali Fazal, Divyendu Sharma",
       rating: "8.4/10",
       year: "2018",
       description: "A shocking incident at a wedding procession ignites a series of events entangling the lives of two families in the lawless city of Mirzapur.",
-      image: "https://m.media-amazon.com/images/M/MV5BMjEzNzMwNjAzMl5BMl5BanBnXkFtZTgwMTI4MzY3NjM@._V1_.jpg"
+      image: "https://m.media-amazon.com/images/M/MV5BMjEzNzMwNjAzMl5BMl5BanBnXkFtZTgwMTI4MzY3NjM@._V1_.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=ZNeGF-PvRHY",
     },
 
     {
@@ -286,8 +311,9 @@ var movieList = {
       cast: "Arunoday Singh",
       rating: "8.3/10",
       year: "2018",
-      description: "Rudra Srivastava, a senior inspector with Uttarakhand police is lured into kidnapping a young girl at her request. The plot begins as a simple plan to extort money in exchange for her release.",
-      image: "https://img.starbiz.com/resize/750x-/2020/09/23/apaharan-web-series-download-1-6592.jpg"
+      description: "Rudra Srivastava, a senior inspector with Uttarakhand police is lured into kidnapping a young girl at her request.",
+      image: "https://img.starbiz.com/resize/750x-/2020/09/23/apaharan-web-series-download-1-6592.jpg",
+      youtubeLink: "https://www.youtube.com/watch?v=CxNYucYVEkQ",
     }
   ]
 
@@ -351,6 +377,7 @@ function App() {
                     <p>IMDB - {movies.rating}</p>
                     <p>Year - {movies.year}</p>
                     <p>{movies.description}</p>
+                    <a id="btn" href={movies.youtubeLink} target="_blank">Watch Trailer!</a>
                   </div>
                 </div>
               </div>
